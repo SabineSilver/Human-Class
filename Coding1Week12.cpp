b55180711b3eb375cd2032382b0077cac5e23a72
SabineSilver <silviaplata2751@gmail.com>
2025-04-17 11:28:23 -0500

    Added child classes to parent human class

--- Coding1Week12.cpp
+++ Coding1Week12.cpp
@@ -7,10 +7,10 @@
 using namespace std;
 
 class human {
-private:
+protected:          //change from private to allow child classes to inherit these variables
     string name;
-    int health = 10;
-    int damage = 4;
+    int health;
+    int damage;
 
 public:
     human(string, int, int);
@@ -25,11 +25,13 @@ public:
         }
         else {
             health = givenHealth; 
+            cout << name << " has " << health << " health left!\n";
         }
     }
     void ChangeHealth(int by = 1) {
-        health += by;
-        SetHealth(health);
+        int tempHealth = health;
+        tempHealth += by;
+        SetHealth(tempHealth);
     }
     int GetHealth() {
         return health;
@@ -80,18 +82,57 @@ void human::SayHello() {
     cout << "Hi! My name is " << name << " and my health is " << health << "!\n";
 }
 
+//the wizard class inherits from the human class.
+//everything that the human class has, so does the wizard
+class wizard : public human {
+public:
+    int mana;
+
+    void castSpell() {
+        cout << name << " casts a spell!\n";
+    }
+
+    //build a spell that poisons any human
+    //it wil cut their health in half
+    void poison(human& target) {
+        cout << "The terrible wizard " << name << " has poisoned " << target.GetName() << "!!\n";
+        int halfOfTargetHealth = target.GetHealth() / 2;
+        target.ChangeHealth(-halfOfTargetHealth);
+    }
+};
+
+class barbarian : public human {
+public:
+    int rage;
+
+    void hitSomething() {
+        cout << name << " hits something!\n";
+    }
+};
 
 
 int main()
 {
-    human ken;
 
+
+    wizard dumblydore;
+    dumblydore.SetName("Abloose");
+    dumblydore.SayHello();
+    dumblydore.castSpell();
+
+    barbarian drax;
+    drax.SetName("Drax the Destroyer");
+    drax.SayHello();
+    drax.hitSomething();
+
+    human ken;
     ken.SetName("Ken");
     
     cout << "This human says their name is " << ken.GetName() << "!\n";
     cout << "Say hello " << ken.GetName() << ".\n";
     ken.SayHello();
 
+    dumblydore.poison(drax);
 }
 
 /*
